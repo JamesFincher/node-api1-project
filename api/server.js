6b7855a94b5280f4fa0c60ba450de4bdf0516930
heartandkarma <heartandkarma@gmail.com>
2022-05-10 02:02:13 -0700

    all tests passing

--- api/server.js
+++ api/server.js
@@ -50,7 +50,30 @@ server.delete("/api/users/:id", (req, res) => {
   });
 });
 
-server.put("/api/users/:id", (res, req) => {
-  model.update(req.params.id, req.body).then(res.status(202).json(result));
+server.put("/api/users/:id", (req, res) => {
+  const user = req.body;
+  const id = req.params.id;
+
+  model
+    .update(id, user)
+    .then((possibleUser) => {
+      if (!possibleUser) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist" });
+      } else if (!user.name || !user.bio) {
+        res
+          .status(400)
+          .json({ message: "Please provide name and bio for the user" });
+      } else {
+        res.json(possibleUser);
+      }
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: "The user information could not be modified" });
+    });
 });
+
 module.exports = server;
