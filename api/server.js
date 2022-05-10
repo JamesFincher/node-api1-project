0a99d6e0ea46a0068cfd32d6cf8048827e04719a
heartandkarma <heartandkarma@gmail.com>
2022-05-10 01:25:26 -0700

    got 12 working. Needing to fix put.

--- api/server.js
+++ api/server.js
@@ -1,3 +1,56 @@
-// BUILD YOUR SERVER HERE
+const express = require("express");
+const server = express();
+const model = require("./users/model");
+server.use(express.json());
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get("/api/users", (req, res) => {
+  model.find().then((users) => {
+    res.json(users);
+  });
+});
+
+//get by id
+
+server.get("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  model.findById(id).then((result) => {
+    if (result == null) {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist" });
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+//post user
+server.post("/api/users/", (req, res) => {
+  model.insert(req.body).then((result) => {
+    const user = req.body;
+    if (!user.name || !user.bio) {
+      res
+        .status(400)
+        .json({ message: "Please provide name and bio for the user" });
+    } else {
+      res.status(201).json(result);
+    }
+  });
+});
+server.delete("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  model.remove(id).then((result) => {
+    if (result == null) {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist" });
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+server.put("/api/users/:id", (res, req) => {
+  model.update(req.params.id, req.body).then(res.status(202).json(result));
+});
+module.exports = server;
